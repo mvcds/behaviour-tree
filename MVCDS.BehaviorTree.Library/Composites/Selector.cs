fa3eede807f17ad37859a2ae76c8ae25e8292dce
Marcos Vinícius Correia da Silva <mar_vinc@ig.com.br>
2015-07-21 17:55:47 -0300

    Selector

---
+++ MVCDS.BehaviorTree.Library/Composites/Selector.cs
@@ -0,0 +1,32 @@
+﻿using MVCDS.BehaviorTree.Library.Archetypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCDS.BehaviorTree.Library.Composites
+{
+    public sealed class Selector : Composite
+    {
+        public Selector(bool random = false)
+            : base(random)
+        {
+        }
+
+        protected override NodeStatus Process()
+        {
+            if (IsEmpty)
+                return NodeStatus.Success;
+
+            foreach (INode node in Nodes)
+            {
+                NodeStatus result = node.Process();
+                if (result != NodeStatus.Running)
+                    return result;
+            }
+
+            return NodeStatus.Running;
+        }
+    }
+}
