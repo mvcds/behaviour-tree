8945cb223b9c934f6309efd7471df6360a443806
Marcos Silva <mvcds1@hotmail.com>
2015-07-26 14:20:09 -0300

    Behaviour is tested

---
+++ MVCDS.BehaviorTree.Library.Test/Behaviour_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace MVCDS.BehaviorTree.Library.Test
+{
+    [TestClass]
+    public class Behaviour_Test
+    {
+        [TestMethod]
+        public void No_Root()
+        {
+            try
+            {
+                Behaviour behaviour = new Behaviour(null);
+            }
+            catch(Exception e)
+            {
+                Assert.IsTrue(e is ArgumentNullException);
+            }
+        }
+
+        [TestMethod]
+        public void Node_Fails()
+        {
+            Mock<INode> node = TestHelper.Mock<INode>(NodeStatus.Failure);
+            Behaviour behaviour = new Behaviour(node.Object);
+
+            Assert.AreEqual(NodeStatus.Failure, behaviour.Process());
+        }
+    }
+}
