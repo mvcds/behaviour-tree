f3a4800f7159d7d77a7ae3c28e4eb96278fcf443
Marcos Silva <mvcds1@hotmail.com>
2015-07-26 14:39:00 -0300

    Testing the init method of the leaf

---
+++ MVCDS.BehaviorTree.Library.Test/Leaf_Test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCDS.BehaviorTree.Library.Archetypes;
+using Moq;
+using MVCDS.BehaviorTree.Library.Composites;
+
+namespace MVCDS.BehaviorTree.Library.Test
+{
+    [TestClass]
+    public class Leaf_Test
+    {
+        private class TestableLeaf: Leaf
+        {
+            public TestableLeaf(NodeStatus result)
+            {
+                Result = result;
+            }
+
+            public NodeStatus Result { get; private set; }
+
+            protected override void Init()
+            {
+                base.Init();
+            }
+
+            protected override NodeStatus Process()
+            {
+                return Result;
+            }
+        }
+
+        [TestMethod]
+        public void Executes_Its_Process()
+        {
+            TestableLeaf cud = new TestableLeaf(NodeStatus.Success);
+            TestHelper.AssertProcess(cud, NodeStatus.Success);
+        }
+
+        [TestMethod]
+        public void Executes_Its_When()
+        {
+            TestableLeaf cud = new TestableLeaf(NodeStatus.Success);
+            cud.When<TestableLeaf>(NodeStatus.Success, () => 
+            {
+                cud = new TestableLeaf(NodeStatus.Failure);
+            });
+
+            (cud as INode).Process();
+            TestHelper.AssertProcess(cud, NodeStatus.Failure);
+        }
+
+        [TestMethod]
+        public void Is_Executed_From_Selector()
+        {
+            TestableLeaf cud = new TestableLeaf(NodeStatus.Success);
+            Selector selector = new Selector();
+
+            selector.Add(cud);
+
+            TestHelper.AssertProcess(selector, NodeStatus.Success);
+        }
+    }
+}
