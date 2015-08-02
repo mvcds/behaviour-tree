8bc80618a0612a041e327cec6f9adeb728bd885f
Marcos Silva <mvcds1@hotmail.com>
2015-08-02 19:56:13 -0300

    MVCDS's behaviour documented

--- MVCDS.BehaviorTree.Library/Behaviour.cs
+++ MVCDS.BehaviorTree.Library/Behaviour.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace MVCDS.BehaviorTree.Library
 {
+    /// <summary>
+    /// The status that a node may result
+    /// </summary>
     public enum NodeStatus
     {
         Running,
@@ -18,10 +21,17 @@ namespace MVCDS.BehaviorTree.Library
         NodeStatus Process();
     }
 
+    /// <summary>
+    /// MVCDS' implementation of a behaviour
+    /// </summary>
     public class Behaviour: INode
     {
         private INode Root;
 
+        /// <summary>
+        /// Creates a behaviour
+        /// </summary>
+        /// <param name="root">The root node of the behaviour</param>
         public Behaviour(INode root)
         {
             if (root == null)
@@ -30,6 +40,10 @@ namespace MVCDS.BehaviorTree.Library
             Root = root;
         }
 
+        /// <summary>
+        /// Processes each node
+        /// </summary>
+        /// <returns>The last status of the behaviour</returns>
         public NodeStatus Process()
         {
             NodeStatus result;
