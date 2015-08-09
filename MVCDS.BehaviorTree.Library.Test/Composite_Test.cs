273a3d8f0a2f332feab174f772bdf5c59a90f70d
Marcos Silva <mvcds1@hotmail.com>
2015-08-09 10:27:13 -0300

    Full test coverage

---
+++ MVCDS.BehaviorTree.Library.Test/Composite_Test.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MVCDS.BehaviorTree.Library.Composites;
+
+namespace MVCDS.BehaviorTree.Library.Test
+{
+    [TestClass]
+    public class Composite_Test
+    {
+        [TestMethod]
+        public void Remove_Node()
+        {
+            Mock<INode> node = TestHelper.Mock<INode>();
+            Sequence inverter = new Sequence();
+
+            inverter.Add(node.Object);
+
+            Assert.IsTrue(inverter.Remove(node.Object));
+            Assert.IsFalse(inverter.Remove(node.Object));
+        }
+    }
+}
---
+++ MVCDS.BehaviorTree.Library.Test/Decorator_Test.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MVCDS.BehaviorTree.Library.Archetypes;
+using Moq;
+using MVCDS.BehaviorTree.Library.Decorators;
+
+namespace MVCDS.BehaviorTree.Library.Test
+{
+    [TestClass]
+    public class Decorator_Test
+    {
+        [TestMethod]
+        public void With_Null()
+        {
+            CreateInverter(null);
+        }
+
+        [TestMethod]
+        public void With_Mock()
+        {
+            Mock<INode> node = TestHelper.Mock<INode>();
+            CreateInverter(node.Object);
+        }
+
+        private void CreateInverter(INode node)
+        {
+            try
+            {
+                Inverter inverter = new Inverter(node);
+            }
+            catch (Exception e)
+            {
+                Assert.IsTrue(e is ArgumentNullException);
+            }
+        }
+    }
+}
