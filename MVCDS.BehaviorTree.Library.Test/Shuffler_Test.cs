a18e907e4cf3c9be2dff65d7a1c8ef69ce721431
Marcos Silva <mvcds1@hotmail.com>
2015-08-09 10:13:19 -0300

    The shuffler is covered by the tests

---
+++ MVCDS.BehaviorTree.Library.Test/Shuffler_Test.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using MVCDS.BehaviorTree.Library.Archetypes;
+
+namespace MVCDS.BehaviorTree.Library.Test
+{
+    [TestClass]
+    public class Shuffler_Test
+    {
+        [TestMethod]
+        public void With_Null()
+        {
+            CreateShuffler(null);
+        }
+
+        [TestMethod]
+        public void With_Mock()
+        {
+            Mock<IComposite> composite = new Mock<IComposite>(MockBehavior.Strict);
+            CreateShuffler(composite.Object);
+        }
+
+        private void CreateShuffler(IComposite composite)
+        {
+            try
+            {
+                Shuffler shuffler = new Shuffler(composite);
+            }
+            catch(Exception e)
+            {
+                Assert.IsTrue(e is ArgumentNullException);
+            }
+        }
+    }
+}
